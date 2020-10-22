7fda38c2800f492a649ad80554bb1abb86712b12
sakew <dorinsecu@gmail.com>
2020-10-22 16:02:33 +0300

    pagination and filters added for featured listings

--- listings/views.py
+++ listings/views.py
@@ -12,16 +12,16 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-
     context = {
-        'listings' : paged_listings
+        'listings': paged_listings
 
     }
     return render(request, 'listings/listings.html', context)
 
+
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
+
 def search(request):
     return render(request, 'listings/search.html')
-
