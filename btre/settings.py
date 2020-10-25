ae41bdaabd65af05a14590e3c43e687fd2543fa2
sakew <dorinsecu@gmail.com>
2020-10-25 17:25:30 +0200

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aslkdjas887xccccxxzzz--asdjj&&89'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,8 +85,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
+        'PASSWORD': '',
+        'HOST': '',
     }
 }
 
