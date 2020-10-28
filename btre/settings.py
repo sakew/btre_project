e24162912903309c73104f6e7304b3e0935defa5
sakew <dorinsecu@gmail.com>
2020-10-28 22:10:04 +0200

    testing inquiry and adding "Send Email" functionality to the inquiry modal whenever a new request is submitted from the website.

--- btre/settings.py
+++ btre/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '#$%&N(ASFGAD^*(%326n26835625BEWSRTSER&^@T#%$Bwertb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,8 +87,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
     }
 }
 
@@ -137,3 +139,18 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dorintestdjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'adolex123'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 
@@ -12,7 +13,7 @@ def contact(request):
         phone = request.POST['phone']
         message = request.POST['message']
         user_id = request.POST['user_id']
-        realtor_email = request.POST['realtor_email']
+        realtor_email = request.POST.get('realtor_email', False)
 
         # Check if user has already made an inquiry
         if request.user.is_authenticated:
@@ -26,6 +27,16 @@ def contact(request):
                           phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        # Send Email
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more informations.',
+            'dorintestdjango@gmail.com',
+            [realtor_email, 'dorinsecu@gmail.com'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
